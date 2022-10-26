6741fafdf94bf46f68471c3e24caf77c268bc336
Giulio Giaconi <giulio.giaconi@gmail.com>
2022-10-26 09:54:58 +0100

    added formatted files

--- heapsort.py
+++ heapsort.py
@@ -2,33 +2,33 @@
 
 
 def max_heapify(arr, N, i):
-    l = (i*2) + 1 # left child
-    r = (i*2) + 2 # right child
-    largest = i # initialise largest as root
+    l = (i * 2) + 1  # left child
+    r = (i * 2) + 2  # right child
+    largest = i  # initialise largest as root
 
-    if l<N and arr[l] > arr[i]: # compare root with left child
+    if l < N and arr[l] > arr[i]:  # compare root with left child
         largest = l
-    
-    if r<N and arr[r] > arr[largest]: # compare root with right child
+
+    if r < N and arr[r] > arr[largest]:  # compare root with right child
         largest = r
 
-    if largest !=i:
+    if largest != i:
         arr[largest], arr[i] = arr[i], arr[largest]
-        max_heapify(arr, N, largest) # heapify the current largest element
+        max_heapify(arr, N, largest)  # heapify the current largest element
 
 
 def heapsort(arr):
     l = len(arr)
-    for i in range(l//2-1, -1, -1):
+    for i in range(l // 2 - 1, -1, -1):
         max_heapify(arr, l, i)
 
-    for i in range(l-1, 0, -1): # extract elements
+    for i in range(l - 1, 0, -1):  # extract elements
         arr[i], arr[0] = arr[0], arr[i]
         max_heapify(arr, i, 0)
 
 
-if __name__=="__main__":
-    test_arr = [10,5,4,20,7,2,3,8]
+if __name__ == "__main__":
+    test_arr = [10, 5, 4, 20, 7, 2, 3, 8]
     print(f"original array: {test_arr}")
     heapsort(test_arr)
     print(f"Sorted array: {test_arr}")
--- quicksort.py
+++ quicksort.py
@@ -2,31 +2,32 @@
 
 import random
 
+
 def partition(array, low, high):
 
     pivot = array[high]
-    i = low - 1 # index of highest element in low side
+    i = low - 1  # index of highest element in low side
 
     for j in range(low, high):
         if array[j] <= pivot:
-            i = i+1
+            i = i + 1
             array[j], array[i] = array[i], array[j]
 
-    array[i+1], array[high] = array[high], array[i+1]
+    array[i + 1], array[high] = array[high], array[i + 1]
 
-    return i+1
+    return i + 1
 
-    
+
 def quick_sort(array, low, high):
     if low < high:
 
         part_index = partition(array, low, high)
-        quick_sort(array, low, part_index-1)
-        quick_sort(array, part_index+1, high)
+        quick_sort(array, low, part_index - 1)
+        quick_sort(array, part_index + 1, high)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     array = [10, 7, 8, 9, 1, 5, 20, 15]
     quick_sort(array, 0, len(array) - 1)
-    
-    print(f'Sorted array: {array}')
 
+    print(f"Sorted array: {array}")
