1654c274462f0751732e9cbfba8bb621e89cbf1e
Giulio Giaconi <Giulio.Giaconi@ofcom.org.uk>
2022-11-10 10:59:50 +0000

    slight change

--- quicksort.py
+++ quicksort.py
@@ -10,7 +10,7 @@ def partition(array, low, high):
 
     for j in range(low, high):
         if array[j] <= pivot:
-            i = i + 1
+            i+=1
             array[j], array[i] = array[i], array[j]
 
     array[i + 1], array[high] = array[high], array[i + 1]
@@ -20,7 +20,6 @@ def partition(array, low, high):
 
 def quick_sort(array, low, high):
     if low < high:
-
         part_index = partition(array, low, high)
         quick_sort(array, low, part_index - 1)
         quick_sort(array, part_index + 1, high)
@@ -29,5 +28,4 @@ def quick_sort(array, low, high):
 if __name__ == "__main__":
     array = [10, 7, 8, 9, 1, 5, 20, 15]
     quick_sort(array, 0, len(array) - 1)
-
     print(f"Sorted array: {array}")
